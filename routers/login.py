78a9a00fc9c7afa38f353d3b0b3569245a2a9787
JuniorsQ <quintanajuniors@gmail.com>
2023-10-04 11:45:51 -0400

    act github

--- routers/login.py
+++ routers/login.py
@@ -13,3 +13,7 @@ def login(user: User):
     if user.email =="juniors@gmail.com" and user.password =="admin":
      token: str= create_token(user.dict())
      return JSONResponse(status_code=200, content=token)
+# git remote set-url gitlab https://gitlab.com/quintanajuniors:glpat-C8f_i56RjANi34MzsMfv@gitlab.com/quintanajuniors/movie-api-platzi/
+
+# git remote add gitlab https://oauth2:glpat-C8f_i56RjANi34MzsMfv@gitlab.com/quintanajuniors/movie-api-platzi.git
+# https://gitlab.com//movie-api-platzi
