9e2e7bfd952d3b2009aae416b661422adf1e0cac
JuniorsQ <quintanajuniors@gmail.com>
2023-10-04 15:43:33 -0400

    in deploy changes

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from middlewares.error_handler import ErrorHandler
 from routers.movie import movie_router
 from routers.login import login_router
 
-# if __name__ == "__main__":
-#     uvicorn.run("main:app", host="0.0.0.0",
-#                 port=int(os.environ.get("PORT", 8000)))
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0",
+                port=int(os.environ.get("PORT", 8000)))
 
 
 
