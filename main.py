69d5b796f085b2e69a6a0d674fe394884f5348b1
JuniorsQ <quintanajuniors@gmail.com>
2023-10-04 15:59:21 -0400

    in deploy changes

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from routers.login import login_router
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0",
-                port=int(os.environ.get("PORT", 8000)))
+    port=int(os.environ.get("PORT", 8000)))
 
 
 
